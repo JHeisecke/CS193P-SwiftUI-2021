58cf2476816b8e3a60911a2195849042299e48d4
Javier <jheisecke@bancognb.com.py>
2022-02-10 15:20:55 -0300

    removes theme buttons

--- MemoryGame/MemoryGame/View/MemoryGameView.swift
+++ MemoryGame/MemoryGame/View/MemoryGameView.swift
@@ -34,53 +34,12 @@ struct MemoryGameView: View {
                     }
                 }
             }
-            Spacer()
-            HStack {
-                vehicleTheme
-                Spacer()
-                foodTheme
-                Spacer()
-                animalTheme
-            }
             .font(.largeTitle)
             .padding(.horizontal)
         }
         .padding(.horizontal)
     }
-    
-    var vehicleTheme: some View {
-        Button {
-//            deck = DeckThemes.themeVehicles.shuffled()
-        } label: {
-            VStack {
-                Image(systemName: "car")
-                Text("Vehicles")
-                    .font(.caption)
-            }
-        }
-    }
-    var foodTheme: some View {
-        Button {
-            //deck = DeckThemes.themeFood.shuffled()
-        } label: {
-            VStack {
-                Image(systemName: "fork.knife")
-                Text("Food")
-                    .font(.caption)
-            }
-        }
-    }
-    var animalTheme: some View {
-        Button {
-            //deck = DeckThemes.themeAnimals.shuffled()
-        } label: {
-            VStack {
-                Image(systemName: "tortoise")
-                Text("Animal")
-                    .font(.caption)
-            }
-        }
-    }
+
     
 }
 
